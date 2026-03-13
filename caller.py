bc762c1eed3075a43e1c43d7d29930176133ccc1
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-03-13 09:53:16 -0500

    Lab 5 Prueba 15

--- caller.py
+++ caller.py
@@ -1,8 +1,8 @@
-
-
 # FREEZE CODE BEGIN
 x = int(input())
 y = float(input())
+from mystery_module import transform_data
+result = transform_data(x, y, "quiz_test")
+print(result)
 # FREEZE CODE END
-
 
