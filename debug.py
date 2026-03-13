3874cba18632f6c559d979a247593b027626cba6
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-03-13 10:23:40 -0500

    Lab 5 Prueba 19

--- debug.py
+++ debug.py
@@ -1,19 +1,16 @@
-def find_max(a, b):
-  max_number = a
-  if a > b and a > c:
-    max_number = a
-  elif b > a and b > c:
-    max_number = b
-  else:
-    max_number = c
-
-#FREEZE CODE BEGIN
+# FREEZE CODE BEGIN
 x = int(input("Enter a number:\n"))
 y = int(input("Enter a number:\n"))
 z = int(input("Enter a number:\n"))
-#FREEZE CODE END
+def find_max(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
 
-print("Maximum value:", maximum)
-
-
+maximum = find_max(x, y, z)
+print(f"Maximum value: {maximum}")
+# FREEZE CODE END
 
