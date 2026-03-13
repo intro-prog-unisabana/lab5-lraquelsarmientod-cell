ef94dbd2b21047a2f3cae468d0b06733b02c7ce2
Laura Raquel Sarmiento Deaza <lraquelsarmientod@gmail.com>
2026-03-12 21:07:12 -0500

    Lab Prueba 6

---
+++ modules.py
@@ -0,0 +1,12 @@
+import os
+import math
+# Imprimir directorio actual
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
+num = int(input("Enter an integer: "))
+log_val = math.log2(num)
+print(f"Log base 2 of {num} is: {log_val}")
+floor_val = math.floor(log_val)
+ceil_val = math.ceil(log_val)
+print(f"Floor: {floor_val}")
+print(f"Ceiling: {ceil_val}")
